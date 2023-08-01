fee16a61a16309b2291e08235bb7dbe165f86308
sabuhi.isazade@yahoo.com <sabuhi.isazade@yahoo.com>
2023-08-01 09:33:20 +0200

    creating footer xidmet section

---
+++ pages/xidmet/[xidmet_detail].js
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import Link from "next/link";
+import RelatedBlogs from "@/src/components/blog/relatedBlogs";
+import CategoriesBlog from "@/src/components/blog/categoriesBlog";
+import TagsBlog from "@/src/components/blog/tagsBlog";
+import SocialNetworks from "@/src/components/others/social_ntwrks";
+import ModalStandart from "@/src/components/modal/modal_stand";
+import InputBtnNbTransition from "@/src/components/input/input_btn_nb_transition";
+import Reyler from "@/src/components/main/reyler";
+import Musteriler from "@/src/components/main/musteriler";
+
+import RelatedMediaPosts from "@/src/components/main/relatedMediaPosts";
+function XidmetDetail() {
+  const [responseData, setResponseData] = useState([]);
+  const router = useRouter();
+  const { query } = router;
+  const { xidmet_detail } = query;
+  console.log(xidmet_detail);
+
+  useEffect(() => {
+    axios
+      .post(
+        "https://api.cagir.az/api/serviceInfo/detail",
+        { titleUrl: xidmet_detail },
+        {
+          headers: {
+            "Accept-Language": "az",
+          },
+        }
+      )
+      .then((response) => {
+        // Handle the response data
+        setResponseData(response.data.result);
+      })
+      .catch((error) => {
+        // Handle any errors
+        console.error(error);
+      });
+  }, [xidmet_detail]);
+  console.log(responseData);
+
+  const {
+    imageUrl,
+    insertDate,
+    serviceInfoNames,
+    serviceInfoTags,
+    titleUrl,
+    viewCount,
+  } = responseData;
+  console.log(serviceInfoTags);
+
+  const { description, shortDescription, title } =
+    responseData.serviceInfoNames && responseData.serviceInfoNames.length > 0
+      ? responseData.serviceInfoNames[0]
+      : {};
+
+  return (
+    <div
+      className="flex flex-col items-center 
+    py-[15px] lg:py-[30px]"
+    >
+      <div className="flex flex-col w-full lg:w-4/5 gap-y-[30px] lg:gap-y-[60px]">
+        <div
+          className="flex flex-col pb-[30px] lg:pb-0 
+      drop-shadow-card lg:drop-shadow-none lg:hover:drop-shadow-card transition duration-300"
+        >
+          <Image
+            width={300}
+            height={300}
+            src={`https://api.cagir.az${imageUrl}`}
+            alt={titleUrl}
+            className="rounded-[20px] w-full aspect-[300/164] lg:aspect-[908/499]"
+          />
+          <div className="flex flex-row justify-between pt-[5px] lg:pt-[30px] pb-[15px] lg:pb-[30px]">
+            <p className="font-medium lg:font-semibold text-[8px] lg:text-[14px] leading-[12px] lg:leading-[21px] text-gray900">
+              {insertDate ? insertDate.slice(0, 10) : ""}
+            </p>
+            <p className="font-semibold text-[10px] lg:text-[18px] leading-[15px] lg:leading-[27px] text-cagiraz">
+              Baxış: {viewCount}
+            </p>
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: description }} />
+          
+        </div>
+        <div>
+        <h4
+            className="font-semibold lg:font-bold text-[16px] lg:text-[20px] lg:leading-[30px] leading-[24px] pb-[15px] pt-[30px] lg:pt-0 text-center lg:text-start
+            border-t border-[#EAEAEA] lg:border-none"
+          >
+            Təqlər
+          </h4>
+          {serviceInfoTags && serviceInfoTags.length > 0 ? (
+            <div className="flex flex-row flex-wrap gap-[10px] lg:gap-[15px]">
+              {serviceInfoTags.map(({ id, name, index }) => (
+                <div key={index}>
+                  <Link href={`/xidmet/tag/${name}`}>
+                    <p
+                      className="font-medium lg:font-semibold text-[8px] lg:text-[10px] leading-[12px] lg:leading-[15px]
+                    border border-cagiraz rounded-[5px] py-[2px] lg:py-[4px] px-[8px] lg:px-[10px] text-cagiraz"
+                    >
+                      {name}
+                    </p>
+                  </Link>
+                </div>
+              ))}
+            </div>
+          ) : (
+            ""
+          )}
+          </div>
+          <Reyler parentId={1} />
+          <Musteriler />
+      </div>
+    </div>
+  );
+}
+
+export default XidmetDetail;
---
+++ pages/xidmet/tag/[tagName].js
@@ -0,0 +1,117 @@
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import axios from "axios";
+import Image from "next/image";
+import Link from "next/link";
+import views from "@/icons/bloq/views.svg";
+
+function XidmetTag() {
+  const router = useRouter();
+  const { query } = router;
+  const { tagName } = query;
+  console.log(tagName);
+  const [responseData, setResponseData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://api.cagir.az/api/serviceInfo/getAll?tagName=${tagName}`, {
+        headers: {
+          "Accept-Language": "az",
+        },
+      })
+      .then((response) => {
+        // Handle the response data
+        setResponseData(response.data.result);
+      })
+      .catch((error) => {
+        // Handle any errors
+        console.error(error);
+      });
+  }, []);
+  console.log(responseData);
+  return (
+    <div className="py-[15px] lg:py-[30px]">
+      <h2 className="my-h2 mb-[15px] lg:mb-[30px] text-center">Xidmətlər</h2>
+      {/* <div className="flex justify-center">
+        <SearchInputMd />
+      </div> */}
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-[10px] lg:gap-[60px] px-[10px] justify-between">
+        {responseData.map(
+          ({
+            index,
+            imageUrl,
+            insertDate,
+            serviceInfoNames,
+            id,
+            viewCount,
+            titleUrl,
+          }) => (
+            <div key={index}>
+              <div className="drop-shadow-card lg:drop-shadow-none hover:drop-shadow-card transition duration-300 bg-white p-[15px] sm:p-[18px] md:p-[21px] lg:p-[24px] lx:p-[27px] 2xl:p-[30px] rounded-[20px] 2xl:rounded-[25px]">
+                <Link href={`/xidmet/${titleUrl}`}>
+                  <Image
+                    width={360}
+                    height={257}
+                    src={`https://api.cagir.az${imageUrl}`}
+                    alt={serviceInfoNames}
+                    className="rounded-[10px] lg:rounded-[20px] w-full aspect-[360/257]"
+                  />
+                </Link>
+                <div className="flex justify-between mt-[10px] lg:mt-[15px] items-center">
+                  <p
+                    className="font-semibold text-[10px] lg:text-[14px] leading-[15px] 
+              lg:leading-[21px] text-gray900 lg:text-gray400"
+                  >
+                    {insertDate.slice(0, 10)}
+                  </p>
+                  <div className="ml-auto border border-cagiraz rounded-lg">
+                    {/* <Link href={`/media/media-detail/${id}`}>
+                    <p className="font-semibold	text-[10px] leading-[15px] text-cagiraz px-[10px] py-[4px] ">
+                      categoryName
+                    </p>
+                  </Link> */}
+                  </div>
+                </div>
+                <Link href={`/xidmet/${titleUrl}`}>
+                  <h5 className="my-h5 mt-[5px] lg:mt-[15px]">
+                    {serviceInfoNames[0].title}
+                  </h5>
+                </Link>
+                <p
+                  className="font-semibold text-[10px] lg:text-[14px] leading-[15px] 
+            lg:leading-[21px] text-gray900 mt-[5px] "
+                >
+                  {serviceInfoNames[0].shortDescription}
+                </p>
+                <div className="flex justify-between mt-[5px] lg:mt-[15px] text-cagiraz">
+                  <div className="flex flew-row justify-center items-center space-x-[5px]">
+                    <Image
+                      className="w-[22px] h-[15px]"
+                      src={views}
+                      alt="views logo"
+                    />
+                    <div>
+                      <p className="font-semibold text-[16px]	lg:text-[18px] leading-[24px] lg:leading-[27px]">
+                        {viewCount}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="ml-auto">
+                    <Link
+                      className="font-extrabold text-[14px] leading-[21px]"
+                      href={`/xidmet/${titleUrl}`}
+                    >
+                      Ətrafli oxu
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default XidmetTag;
