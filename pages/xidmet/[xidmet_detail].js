5d51de99cad016b4c2494e144026f54ffe360c48
sabuhi.isazade@yahoo.com <sabuhi.isazade@yahoo.com>
2023-09-26 17:19:43 +0200

    fixing deployment problem

--- pages/xidmet/[xidmet_detail].js
+++ pages/xidmet/[xidmet_detail].js
@@ -19,7 +19,6 @@ function XidmetDetail() {
   const router = useRouter();
   const { query } = router;
   const { xidmet_detail } = query;
-  console.log(xidmet_detail);
 
   useEffect(() => {
     axios
@@ -41,7 +40,6 @@ function XidmetDetail() {
         console.error(error);
       });
   }, [xidmet_detail]);
-  console.log(responseData.metaTitle);
 
   const {
     imageUrl,
@@ -59,64 +57,65 @@ function XidmetDetail() {
 
   return (
     <div>
-      <Head><title>{responseData.metaTitle}</title></Head>
-    <div
-      className="flex flex-col items-center 
+      <Head>
+        <title>{responseData.metaTitle}</title>
+      </Head>
+      <div
+        className="flex flex-col items-center 
     py-[15px] lg:py-[30px]"
-    >
-      <div className="flex flex-col w-full lg:w-4/5 gap-y-[30px] lg:gap-y-[60px]">
-        <div
-          className="flex flex-col pb-[30px] lg:pb-0 
+      >
+        <div className="flex flex-col w-full lg:w-4/5 gap-y-[30px] lg:gap-y-[60px]">
+          <div
+            className="flex flex-col pb-[30px] lg:pb-0 
       drop-shadow-card lg:drop-shadow-none lg:hover:drop-shadow-card transition duration-300"
-        >
-          <Image
-            width={300}
-            height={300}
-            src={`https://api.cagir.az${imageUrl}`}
-            alt={titleUrl}
-            className="rounded-[20px] w-full aspect-[300/164] lg:aspect-[908/499]"
-          />
-          <div className="flex flex-row justify-between pt-[5px] lg:pt-[30px] pb-[15px] lg:pb-[30px]">
-            <p className="font-medium lg:font-semibold text-[8px] lg:text-[14px] leading-[12px] lg:leading-[21px] text-gray900">
-              {insertDate ? insertDate.slice(0, 10) : ""}
-            </p>
-            <p className="font-semibold text-[10px] lg:text-[18px] leading-[15px] lg:leading-[27px] text-cagiraz">
-              Baxış: {viewCount}
-            </p>
-          </div>
-          <div dangerouslySetInnerHTML={{ __html: description }} />
-          
-        </div>
-        <div>
-        <h4
-            className="font-semibold lg:font-bold text-[16px] lg:text-[20px] lg:leading-[30px] leading-[24px] pb-[15px] pt-[30px] lg:pt-0 text-center lg:text-start
-            border-t border-[#EAEAEA] lg:border-none"
           >
-            Təqlər
-          </h4>
-          {serviceInfoTags && serviceInfoTags.length > 0 ? (
-            <div className="flex flex-row flex-wrap gap-[10px] lg:gap-[15px]">
-              {serviceInfoTags.map(({ id, name, index }) => (
-                <div key={index}>
-                  <Link href={`/xidmet/tag/${name}`}>
-                    <p
-                      className="font-medium lg:font-semibold text-[8px] lg:text-[10px] leading-[12px] lg:leading-[15px]
-                    border border-cagiraz rounded-[5px] py-[2px] lg:py-[4px] px-[8px] lg:px-[10px] text-cagiraz"
-                    >
-                      {name}
-                    </p>
-                  </Link>
-                </div>
-              ))}
+            <Image
+              width={300}
+              height={300}
+              src={`https://api.cagir.az${imageUrl}`}
+              alt={titleUrl}
+              className="rounded-[20px] w-full aspect-[300/164] lg:aspect-[908/499]"
+            />
+            <div className="flex flex-row justify-between pt-[5px] lg:pt-[30px] pb-[15px] lg:pb-[30px]">
+              <p className="font-medium lg:font-semibold text-[8px] lg:text-[14px] leading-[12px] lg:leading-[21px] text-gray900">
+                {insertDate ? insertDate.slice(0, 10) : ""}
+              </p>
+              <p className="font-semibold text-[10px] lg:text-[18px] leading-[15px] lg:leading-[27px] text-cagiraz">
+                Baxış: {viewCount}
+              </p>
             </div>
-          ) : (
-            ""
-          )}
+            <div dangerouslySetInnerHTML={{ __html: description }} />
+          </div>
+          <div>
+            <h4
+              className="font-semibold lg:font-bold text-[16px] lg:text-[20px] lg:leading-[30px] leading-[24px] pb-[15px] pt-[30px] lg:pt-0 text-center lg:text-start
+            border-t border-[#EAEAEA] lg:border-none"
+            >
+              Təqlər
+            </h4>
+            {serviceInfoTags && serviceInfoTags.length > 0 ? (
+              <div className="flex flex-row flex-wrap gap-[10px] lg:gap-[15px]">
+                {serviceInfoTags.map(({ id, name, index }) => (
+                  <div key={index}>
+                    <Link href={`/xidmet/tag/${name}`}>
+                      <p
+                        className="font-medium lg:font-semibold text-[8px] lg:text-[10px] leading-[12px] lg:leading-[15px]
+                    border border-cagiraz rounded-[5px] py-[2px] lg:py-[4px] px-[8px] lg:px-[10px] text-cagiraz"
+                      >
+                        {name}
+                      </p>
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              ""
+            )}
           </div>
           <Reyler parentId={1} />
           <Musteriler />
+        </div>
       </div>
-    </div>
     </div>
   );
 }
